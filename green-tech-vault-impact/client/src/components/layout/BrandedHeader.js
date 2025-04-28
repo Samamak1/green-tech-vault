a196d670fd69c09d37a449f729695f48279c78e1
Lameyer4 <leilaameyer2@gmail.com>
2025-04-27 21:58:51 -0700

    black header removal

--- green-tech-vault-impact/client/src/components/layout/BrandedHeader.js
+++ green-tech-vault-impact/client/src/components/layout/BrandedHeader.js
@@ -7,97 +7,9 @@ import Logo from '../branding/Logo';
 const BrandedHeader = ({ userName = "Anna Katrina Marchesi", userRole = "Head of Administrator", userAvatar = null }) => {
   const theme = useTheme();
   
+  // Return an empty fragment - removing the black bar completely
   return (
-    <AppBar position="static" sx={{ 
-      bgcolor: '#1e1e1e', 
-      boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.1), 0px 4px 5px 0px rgba(0,0,0,0.07), 0px 1px 10px 0px rgba(0,0,0,0.06)', 
-      borderBottom: '1px solid rgba(255,255,255,0.1)' 
-    }} elevation={0}>
-      <Toolbar>
-        <Container maxWidth="xl" sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', py: 1 }}>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <Logo variant="light" size="medium" showTagline={false} />
-          </Box>
-          
-          <Box sx={{ 
-            display: 'flex', 
-            alignItems: 'center',
-            gap: 2
-          }}>
-            {/* Search Box */}
-            <Box sx={{ 
-              position: 'relative',
-              borderRadius: 1,
-              border: '1px solid #8a9a5b',
-              bgcolor: 'transparent',
-              width: 300,
-              display: 'flex',
-              alignItems: 'center'
-            }}>
-              <Box sx={{ 
-                p: 1,
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center'
-              }}>
-                <SearchIcon sx={{ color: '#8a9a5b' }} />
-              </Box>
-              <InputBase
-                placeholder="Search here"
-                sx={{
-                  color: 'rgba(255,255,255,0.7)',
-                  width: '100%',
-                  '& .MuiInputBase-input': {
-                    p: 1,
-                  },
-                }}
-              />
-            </Box>
-            
-            {/* User Info */}
-            <Box sx={{ 
-              display: 'flex', 
-              alignItems: 'center',
-              gap: 1.5
-            }}>
-              <Box sx={{ textAlign: 'right' }}>
-                <Typography variant="subtitle1" sx={{ color: 'white', fontWeight: 500 }}>
-                  {userName}
-                </Typography>
-                <Typography variant="caption" sx={{ 
-                  color: 'white', 
-                  background: theme.palette.background.gradient,
-                  px: 1, 
-                  py: 0.5, 
-                  borderRadius: 1,
-                  display: 'inline-flex',
-                  alignItems: 'center',
-                  gap: 0.5
-                }}>
-                  <Box component="span" sx={{ 
-                    width: 8, 
-                    height: 8, 
-                    borderRadius: '50%', 
-                    bgcolor: 'white',
-                    display: 'inline-block'
-                  }}></Box>
-                  {userRole}
-                </Typography>
-              </Box>
-              <Avatar 
-                src={userAvatar} 
-                alt={userName}
-                sx={{ 
-                  width: 48, 
-                  height: 48,
-                  border: '2px solid #8a9a5b'
-                }}
-              />
-            </Box>
-          </Box>
-        </Container>
-      </Toolbar>
-    </AppBar>
+    <></>
   );
 };
 
